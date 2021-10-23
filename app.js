bb653904488a5d495922eb02600782d7af5e3a80
lizsjh <37032480+lizsjh@users.noreply.github.com>
2021-10-23 11:21:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,28 @@ const response=new Array();
 botui.message.add({
     delay:500,
     loading: true,
-    content: 'Hello. This is Taylor, and I am a bot created by the customer service department.'
+    content: 'Hello. I am a customer service bot.'
 }).then(function(){
     return botui.message.add({
         delay:700,
         loading: true,
         content:'I am handling your request today. What brings you here?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:900,
+        loading: true,
+        content:'I can help you with that. First, could you tell me why you need to replace or return this textbook in more details?'
     });
 }).then(function(){
     return botui.action.text({
@@ -32,16 +48,15 @@ botui.message.add({
         action: {
           placeholder: 'Enter your message.'
         }
-    
-    });
+        });
 }).then(function (res) { 
     console.log(res.value);
     response.push(res.value);
 }).then(function(){
     return botui.message.add({
-        delay:700,
+        delay:1000,
         loading: true,
-        content:'I can help you with that. First, could you tell me your order number?'
+        content:'I still did not get what you said. Can you type again?'
     });
 }).then(function(){
     return botui.action.text({
@@ -56,81 +71,46 @@ botui.message.add({
     return botui.message.add({
         delay:700,
         loading: true,
-        content:'Got it. Please allow me few seconds for pulling up your order.'
+        content:'Got it. Could you input your order number below?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+        });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:700,
+        loading: true,
+        content:'Alright. I will process your request. Please give me a moment.'
     });
 }).then(function(){
     return botui.message.add({
         delay:1200,
         loading: true,
-        content:'Which item(s) is missing?'
+        content:'We are having a problem because of our server update. It is taking longer than usual to retrieve your information and complete your request. Please wait...'
     });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-        });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
 }).then(function(){
     return botui.message.add({
-        delay:900,
+        delay:2000,
         loading: true,
-        content:'I do not quite get what you are saying. Please repeat.'
+        content:'We are still retrieving your information. It is going to take a minute or so. Please continue to wait.'
     });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-        });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
 }).then(function(){
     return botui.message.add({
-        delay:800,
+        delay:3000,
         loading: true,
-        content:'Can you describe conditions of the rest of the items?'
-    });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-    
-    });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
-}).then(function(){
-    return botui.message.add({
-        delay:800,
-        loading: true,
-        content:'I cannot process your message. Please type again.'
-    });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-    
-    });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
-}).then(function(){
-    return botui.message.add({
-        delay:900,
-        loading: true,
-        content:'Thank you for telling me. Meanwhile, I’ve identified the problem: there was a miscommunication in the packaging process.'
+        content:'We have retrieved your information. The 3rd edition is currently in stock.'
     });
 }).then(function(){
     return botui.message.add({
         delay:1200,
         loading: true,
-        content:'I can create a new order that will be delivered within a day, or I can refund for the missing item. What would you prefer?'
+        content:'For your information, shipping will be free, and you need to pay $50 more. Would you still like to exchange the book?'
     });
 }).then(function(){
     return botui.action.text({
@@ -142,33 +122,11 @@ botui.message.add({
 }).then(function (res) { 
     console.log(res.value);
     response.push(res.value);
-}).then(function(){
-    return botui.message.add({
-        delay:700,
-        loading: true,
-        content:'I cannot understand. Can you repeat?'
-    });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-    
-    });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
-}).then(function(){
-    return botui.message.add({
-        delay:700,
-        loading: true,
-        content:'Alright. I will process your request. Please hold on for a moment.'
-    });
 }).then(function(){
     return botui.message.add({
         delay:1800,
         loading: true,
-        content:'I have processed your request. The issue is resolved'
+        content:'I have processed your request. The issue is resolved.'
     });
 }).then(function(){
     sendcomplete();
@@ -180,5 +138,5 @@ botui.message.add({
 });
 
 function sendcomplete(){
-    window.parent.postMessage({"message": "completed","text":response}, "*");
+    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2],"text4":response[3],"text5":response[4],"text6":response[5]}, "*");
 };

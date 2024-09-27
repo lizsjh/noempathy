ffd0de864920e46ae526eced4464ef77a9ae4598
lizsjh <37032480+lizsjh@users.noreply.github.com>
2024-09-27 14:09:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -11,14 +11,13 @@ botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'I am handling your request today. What brings you here? If this is about your order, please provide your order number as well.'
+        content:'I am handling your request today. What brings you here?'
     });
 }).then(function(){
     return botui.action.text({
         action: {
           placeholder: 'Enter your message.'
         }
-    
     });
 }).then(function (res) { 
     console.log(res.value);
@@ -28,14 +27,13 @@ botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'I can help you with that. First, could you tell me why you need to replace or return this textbook?'
+        content:'I can help you with that. First, could you tell me your order number?'
     });
 }).then(function(){
     return botui.action.text({
         action: {
           placeholder: 'Enter your message.'
         }
-    
     });
 }).then(function (res) { 
     console.log(res.value);
@@ -45,21 +43,20 @@ botui.message.add({
         delay:2000,
         loading: true,
         photo: true,
-        content:'Got it. Please give me a moment while pulling up your order.'
+        content:'Alright. Please give me a moment while pulling up your order.'
     });
 }).then(function(){
     return botui.message.add({
         delay:6000,
         loading: true,
         photo: true,
-        content:'The 2nd edition is currently in stock. It costs $125 more. Please confirm if you would like to proceed with the exchange. '
+        content:'You have ordered one pair of jeans, one swimsuit, and one necklace. Can you confirm?'
     });
 }).then(function(){
     return botui.action.text({
         action: {
           placeholder: 'Enter your message.'
         }
-    
     });
 }).then(function (res) { 
     console.log(res.value);
@@ -69,55 +66,64 @@ botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'Oh, in fact, I pulled up the information about a wrong textbook.'
+        content:'Oh, in fact, you ordered a different set of products. I accidentally pulled up the wrong order.'
     });
 }).then(function(){
     return botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'I am searching for the correct information again. Please hold on.'
-    });
-}).then(function(){
-    return botui.message.add({
-        delay:30000,
-        loading: true,
-        photo: true,
-        content:'The 3rd edition is currently in stock. You just have to pay $50 more for the exchange. Would you still like to exchange the book? '
-    });
-}).then(function(){
-    return botui.action.text({
-        action: {
-          placeholder: 'Enter your message.'
-        }
-        });
-}).then(function (res) { 
-    console.log(res.value);
-    response.push(res.value);
-}).then(function(){
-    return botui.message.add({
-        delay:4000,
-        loading: true,
-        photo: true,
-        content:'Alright. Please give me a moment.'
+        content:'I am searching for your order again. Please hold on.'
     });
 }).then(function(){
     return botui.message.add({
         delay:10000,
         loading: true,
         photo: true,
-        content:'I have processed your request. The issue is resolved.'
+        content:'Your order includes one pair of jeans, one sweater, and one baseball cap. Please confirm.'
     });
 }).then(function(){
-    sendcomplete();
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
     return botui.message.add({
         delay:4000,
         loading: true,
         photo: true,
-        content:'Please contact us again if you need further assistance. Bye.'
+        content:'Which item is missing?'
+    });
+}).then(function(){
+    return botui.action.text({
+        action: {
+          placeholder: 'Enter your message.'
+        }
+    });
+}).then(function (res) { 
+    console.log(res.value);
+    response.push(res.value);
+}).then(function(){
+    return botui.message.add({
+        delay:5000,
+        loading: true,
+        photo: true,
+        content:'I can create a new order for the missing item to be delivered within a day. Please give me a moment.'
+    });
+}).then(function(){
+    sendcomplete();
+    return botui.message.add({
+        delay:10000,
+        loading: true,
+        photo: true,
+        content:'It is taking longer than usual because I am still gathering some information to process your request.'
     });
 });
 
 function sendcomplete(){
-    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2], "text4":response[3]}, "*");
+    window.parent.postMessage({"message": "completed","text1":response[0],"text2":response[1],"text3":response[2], "text4":response[3], "text5":response[4]}, "*");
 };
